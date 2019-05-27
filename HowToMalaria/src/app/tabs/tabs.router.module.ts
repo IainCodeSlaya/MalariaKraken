bdce7696ab71fcc645fa776a5dbd2ec798f3d2e4
Iain Steyn <u16007192@tuks.co.za>
2019-05-27 03:31:04 +0200

    3rd commit

--- HowToMalaria/src/app/tabs/tabs.router.module.ts
+++ HowToMalaria/src/app/tabs/tabs.router.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
+import { AddPageModule } from '../add/add.module';
+import { EditPageModule } from '../edit/edit.module';
+import { HomePageModule } from '../home/home.module';
+import { DetailsPageModule } from '../details/details.module';
 
 const routes: Routes = [
   {
@@ -8,42 +12,35 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab1/tab1.module#Tab1PageModule'
-          }
-        ]
-      },
-      {
-        path: 'tab2',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab2/tab2.module#Tab2PageModule'
-          }
-        ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab3/tab3.module#Tab3PageModule'
-          }
-        ]
-      },
-      {
         path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
+        redirectTo: '/tabs/(home:home)',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        outlet: 'home',
+        component: HomePage
+      },
+      {
+        path: 'add',
+        outlet: 'add',
+        component: AddPage
+      },
+      {
+        path: ':id',
+        outlet: 'edit',
+        component: EditPage
+      },
+      {
+        path: ':id',
+        outlet: 'details',
+        component: DetailsPage
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/(home:home)',
     pathMatch: 'full'
   }
 ];
